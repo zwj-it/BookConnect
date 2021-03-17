469a5c7d9decbbbc5a49bc8de8cf2232f444f689
Heyuzj <55737663+Heyuzj@users.noreply.github.com>
2021-03-17 21:16:39 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bluebell/controllers"
+	"bluebell/dao/mysql"
+	"bluebell/dao/redis"
+	"bluebell/logger"
+	"bluebell/pkg/snowflake"
+	"bluebell/routes"
+	"bluebell/settings"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"go.uber.org/zap"
+)
+
+// @title Zwj_bluebell
+// @version 1.0
+// @description 韭菜社区
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name 474527746@qq.com
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host 127.0.0.1:8084
+// @BasePath /api/v1
+func main() {
+	//1.加载配置
+	if err := settings.Init(); err != nil {
+		fmt.Printf("init settings failed, err:%v\n", err)
+		return
+	}
+	//2.初始化日志
+	if err := logger.Init(settings.Conf.LogConfig, settings.Conf.Mode); err != nil {
+		fmt.Printf("init logger failed, err:%v\n", err)
+		return
+	}
+	defer zap.L().Sync() //把缓冲区的日志追加
+	//zap.L().Debug("logger init success")
+	//3.初始化MySQL连接
+	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+	defer mysql.Close()
+	//4.初始化Redis连接
+	if err := redis.Init(settings.Conf.RedisConfig); err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+		return
+	}
+	defer redis.Close()
+	if err := snowflake.Init(settings.Conf.StartTime, settings.Conf.MachineID); err != nil {
+		fmt.Printf("init snowflake failed,err:%v\n", err)
+		return
+	}
+	//snowflake.GenID()
+	//初始化gin框架内置效验器的翻译器
+	if err := controllers.InitTrans("zh"); err != nil {
+		fmt.Printf("init trans failed,err:%v\n", err)
+		return
+	}
+	//5.注册路由
+	r := routes.Setup(settings.Conf.Mode)
+	err := r.Run(fmt.Sprintf(":%d", settings.Conf.Port))
+	if err != nil {
+		fmt.Printf("run server failed, err:%v\n", err)
+		return
+	}
+	////6.启动服务（优雅关机）
+	//srv := &http.Server{
+	//	Addr:    fmt.Sprintf(":%d", settings.Conf.Port),
+	//	Handler: r,
+	//}
+	//fmt.Println(settings.Conf.Port)
+	//
+	//go func() {
+	//	// 开启一个goroutine启动服务
+	//	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	//		zap.L().Fatal("Server Shutdown: ", zap.Error(err))
+	//	}
+	//}()
+	//
+	//// 等待中断信号来优雅地关闭服务器，为关闭服务器操作设置一个5秒的超时
+	//quit := make(chan os.Signal, 1) // 创建一个接收信号的通道
+	//// kill 默认会发送 syscall.SIGTERM 信号
+	//// kill -2 发送 syscall.SIGINT 信号，我们常用的Ctrl+C就是触发系统SIGINT信号
+	//// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
+	//// signal.Notify把收到的 syscall.SIGINT或syscall.SIGTERM 信号转发给quit
+	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) // 此处不会阻塞
+	//<-quit                                               // 阻塞在此，当接收到上述两种信号时才会往下执行
+	//zap.L().Info("Shutdown Server ...")
+	//// 创建一个5秒超时的context
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
+	//// 5秒内优雅关闭服务（将未处理完的请求处理完再关闭服务），超过5秒就超时退出
+	//if err := srv.Shutdown(ctx); err != nil {
+	//	zap.L().Fatal("Server Shutdown: ", zap.Error(err))
+	//}
+	//
+	//zap.L().Info("Server exiting")
+}
